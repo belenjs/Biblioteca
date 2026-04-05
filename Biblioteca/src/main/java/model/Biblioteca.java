8b9a3828b91b53abbc15dd48ffe6faee93ea6cb7
Belén Jiménez <113296132+belenjs@users.noreply.github.com>
2026-04-05 10:09:39 +0200

    feat: crea clase Biblioteca para recibir la lista de libros

--- Biblioteca/src/main/java/model/Biblioteca.java
+++ Biblioteca/src/main/java/model/Biblioteca.java
@@ -1,5 +1,29 @@
 package model;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private List<Libro> listaLibros;
+
+    public Biblioteca() {
+        listaLibros = new ArrayList<>();
+    }
+
+    public Biblioteca(List<Libro> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+
+    @Override
+    public String toString(){
+        return "La lista de libros de la Biblioteca es: "+listaLibros+'}';
+    }
+
+    public List<Libro> getListaLibros() {
+        return listaLibros;
+    }
+
+    public void setListaLibros(List<Libro> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
 }
